0dd20ea88c562e3d45b3280bc1c013c90a774b91
EC2 Default User <ec2-user@ip-172-31-20-1.ap-northeast-1.compute.internal>
2023-01-22 05:43:01 +0000

    first-commit

---
+++ eight/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //(以下の内容をJavaで記述してく)
+        //勇者よ、この仮想世界に生まれよ！
+        //お化けキノコよ、この仮装世界に生まれよ！
+        //勇者よ、戦え！
+        //お化けキノコよ、逃げろ！
+    }
+}
