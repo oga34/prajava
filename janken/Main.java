086ad385df68a562c6558f0f04b21dff8ca76dda
EC2 Default User <ec2-user@ip-172-31-20-1.ap-northeast-1.compute.internal>
2023-01-24 11:49:30 +0000

    じゃんけんプログラム

---
+++ janken/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("じゃんけんを始めます");
+        System.out.print("[0]グー,[1]チョキ,[2]パーの中から番号で選択してください:");
+        String[] number = {"グー","チョキ","パー"};
+        int n = new java.util.Scanner(System.in).nextInt();
+        int c = new java.util.Random().nextInt(3);
+        if (n == c) {
+            System.out.println("コンピューターの手は" + number[c]);
+            System.out.println("あいこでした");
+        } else if ((n == 0 && c == 1) || (n == 1 && c == 2) || (n == 2 && c == 0)) {
+            System.out.println("コンピューターの手は" + number[c]);
+            System.out.println("あなたの勝ち");
+        } else {
+            System.out.println("コンピューターの手は" + number[c]);
+            System.out.println("あなたの負け");
+        } 
+    }
+}
